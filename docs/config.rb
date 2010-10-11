3bf6f6f559790b93d9f3032babaffced51779bab
Thomas Reynolds <tdreyno@gmail.com>
2010-10-11 16:00:52 -0700

    docs

---
+++ docs/config.rb
@@ -0,0 +1,9 @@
+gem 'ultraviolet'
+require 'uv'   
+
+gem 'rack-codehighlighter'
+require 'rack/codehighlighter'
+
+use Rack::Codehighlighter, :ultraviolet, :markdown => true, 
+  :theme => "twilight", :element => "pre>code", 
+  :pattern => /\A:::([-_+\w]+)\s*(\n|&#x000A;)/, :logging => false
