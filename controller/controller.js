789593bca22ba554141ed4868957a1dbaafcf543
Chris Antoine <cantoine@gmail.com>
2011-12-02 14:41:26 -0500

    Update for JMVC 3.2
    I added the old JMVC history plugin and updated the
    controller/subscriber to work with it in the new namespace. I'm not
    sure if this is the best way to handle the update but it was the
    quickest method of getting the router working.

--- controller/controller.js
+++ controller/controller.js
@@ -1,102 +1,102 @@
-steal.plugins("jquery",
-              "jquery/class",
-              "jquery/controller/history",
-              "jquery/lang/openajax",
-              "ss/router/sherpa")
-     .then(function($) {
+steal('jquery',
+    'jquery/class',
+    'jquery/controller/subscribe',
+    'ss/router/controller/history',
+    'jquery/event/hashchange',
+    'jquery/lang/openajax',
+    'ss/router/sherpa')
+    .then(function($) {
 
-  $.Class.extend("SS.Router.Controller",
-  {
-    currentParams: {},
-    URLPrefix:     null,
-    hashSeparator: "#",  // Google suggests #!
-    
-    init: function() {
-      // Global router object
-      Router = new Sherpa.Router();
+        $.Class.extend("SS.Router.Controller",
+        {
+        currentParams: {},
+        URLPrefix:     null,
+        hashSeparator: "#",  // Google suggests #!
 
-      OpenAjax.hub.subscribe("history.**", function(event_name) {
-        //You'll also typically need to match the "index" history, which typically
-        //occurs when no specific url has been entered.  You can route to your
-        //default url here. 
-        if (event_name === 'history.index') {
-          OpenAjax.hub.publish("router.index_page");
-          return;
-        }
+        init: function() {
+            // Global router object
+            Router = new Sherpa.Router();
+            OpenAjax.hub.subscribe("history.**", function(event_name) {
+                //You'll also typically need to match the "index" history, which typically
+                //occurs when no specific url has been entered.  You can route to your
+                //default url here. 
+                if (event_name === 'history.index') {
+                    OpenAjax.hub.publish("router.index_page");
+                    return;
+                }
 
-        var key = event_name.replace(/^history(\.*)/, '/');
-        //@steal-remove-start
-        //steal.dev.log('History event: ' + key);
-        //@steal-remove-end
+                var key = event_name.replace(/^history(\.*)/, '/');
+                //@steal-remove-start
+                //steal.dev.log('History event: ' + key);
+                //@steal-remove-end
 
-        var foundRoute = Router.recognize(key);
-        if (!foundRoute) {
+                var foundRoute = Router.recognize(key);
+                if (!foundRoute) {
+                    //@steal-remove-start
+                    steal.dev.log("WARNING: Router failed to find route for: " + key);
+                    //@steal-remove-end
+                    return;
+                }
+
+                SS.Router.currentParams = foundRoute.params;
+                OpenAjax.hub.publish(foundRoute.destination, foundRoute.params);
+            });
+        },
+
+        /**
+         * Given an html uri, match it to a history point and update the history
+         * of the page to navigate to that location.
+         */
+        routeTo: function(uri) {
           //@steal-remove-start
-          steal.dev.log("WARNING: Router failed to find route for: " + key);
+          //steal.dev.log("ROUTER: Request to route to " + uri);
           //@steal-remove-end
-          return;
+
+          //Remove /dev.php from the uri;
+          if (this.URLPrefix) {
+            uri = uri.replace(this.URLPrefix, '');
+          }
+
+          var uriWithHash = this.hashSeparator + uri;
+
+          if (window.location.hash === uriWithHash) {
+              //@steal-remove-start
+              steal.dev.log("ROUTER: Already at " + uriWithHash);
+              //@steal-remove-end
+              return;
+          }
+
+          var route = Router.recognize(uri);
+          if (!route) {
+              // No route. Let it go. Add a '#' to the end so that we don't infinite redirect.
+              window.location.href = uri + this.hashSeparator;
+              return;
+          } else {
+              //@steal-remove-start
+              //steal.dev.log("ROUTER: Detected appropriate history point: " + uriWithHash);
+              //@steal-remove-end
+          }
+
+          var components = window.location.href.split(this.hashSeparator);
+          if (components.length > 1) {
+            //@steal-remove-start
+            steal.dev.log('ROUTER: ' + window.location.hash + ' -> ' + uriWithHash);
+            //@steal-remove-end
+
+            if (window.location.hash !== uriWithHash) {
+              window.location = components[0] + uriWithHash;
+              return true;
+            } else {
+              return false;
+            }
+          } else { //let's "redirect" to the appropriate javascriptmvc uri
+            var prefix = this.URLPrefix || '';
+            window.location = window.location.protocol + '//' + 
+                              window.location.hostname + prefix + uriWithHash;
+          }
         }
-        
-        SS.Router.currentParams = foundRoute.params;
-        OpenAjax.hub.publish(foundRoute.destination, foundRoute.params);
-      });
-    },
-    
-    /**
-     * Given an html uri, match it to a history point and update the history
-     * of the page to navigate to that location.
-     */
-    routeTo: function(uri) {
-      //@steal-remove-start
-      //steal.dev.log("ROUTER: Request to route to " + uri);
-      //@steal-remove-end
-
-      //Remove /dev.php from the uri;
-      if (this.URLPrefix) {
-        uri = uri.replace(this.URLPrefix, '');
-      }
-      
-      var uriWithHash = this.hashSeparator + uri;
-
-      if (window.location.hash === uriWithHash) {
-        //@steal-remove-start
-        steal.dev.log("ROUTER: Already at " + uriWithHash);
-        //@steal-remove-end
-        return;
-      }
-      
-      var route = Router.recognize(uri);
-      if (!route) {
-        // No route. Let it go. Add a '#' to the end so that we don't infinite redirect.
-        window.location.href = uri + this.hashSeparator;
-        return;
-      } else {
-        //@steal-remove-start
-        //steal.dev.log("ROUTER: Detected appropriate history point: " + uriWithHash);
-        //@steal-remove-end
-      }
-
-      var components = window.location.href.split(this.hashSeparator);
-      if (components.length > 1) {
-        //@steal-remove-start
-        steal.dev.log('ROUTER: ' + window.location.hash + ' -> ' + uriWithHash);
-        //@steal-remove-end
-        
-        if (window.location.hash !== uriWithHash) {
-          window.location = components[0] + uriWithHash;
-          return true;
-        } else {
-          return false;
+        },
+        {
         }
-      } else { //let's "redirect" to the appropriate javascriptmvc uri
-        var prefix = this.URLPrefix || '';
-        window.location = window.location.protocol + '//' + 
-                          window.location.hostname + prefix + uriWithHash;
-      }
-    }
-  },
-  {
-  }
-  );
-
+    );
 });
